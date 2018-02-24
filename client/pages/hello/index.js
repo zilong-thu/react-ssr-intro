e962f72fdd157326a040ef5e2b59fb73d4bbc595
王子龙 <wangzilong03@maoyan.com>
2018-02-24 23:19:26 +0800

    add log

--- client/pages/hello/index.js
+++ client/pages/hello/index.js
@@ -2,6 +2,19 @@
 import React from 'react';
 
 export default class HelloWorldPage extends React.Component {
+  constructor() {
+    super();
+    console.log('\n组件生命周期方法: constructor');
+  }
+
+  componentWillMount() {
+    console.log('组件生命周期方法: componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('组件生命周期方法: componentDidMount');
+  }
+
   render() {
     return <div>Hello World!</div>;
   }
